2cdf57679aceacfa05ec210ba5ec00b5164a1d7c
durraniarts <abdurrehmandurrani1215@gmail.com>
2025-01-26 01:36:43 +0500

    redirect from server-side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-"use client";
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
-  const { data: session } = useSession();
-  const router = useRouter();
-  if (!session) router.push("/login");
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/login");
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
